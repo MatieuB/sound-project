34db086868076f899c0f9c1457592cbcf450ee58
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-03-02 15:56:06 -0700

    have ability to save shows

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-//http://api.bandsintown.com/artists/Skrillex/events.json?api_version=2.0&app_id=Local_Sounds
-
 $(document).ready(function() {
   console.log("ready!");
+  // function addToLocalStorage() {;
+  var showTracker = [];
+  var trackedShow;
+  var showsChecked = $('input:checked');
+// $('.interest').on('click',function(){
+// console.log("box checked");
+// for(var i = 0; i < showsChecked.length; i++) {
+//   trackedShow = ($(this).parent().children());
+//   console.log(trackedShow);
+//   }
+// })
+
+
+  $('tbody').on('change', '.interest', function() {
+    if ($(event.target).is(':checked')) {
+      console.log("checkbox changed");
+      trackedShow = ($(this).parent().children());
+      console.log(trackedShow);
+      showTracker.push('<li>'+trackedShow.text()+'</li>');
+      console.log("show tracker: " + showTracker);
+    } else {
+      showTracker.splice(showTracker.indexOf($(this).parent().children().text()), 1);
+      console.log("unchecked!",showTracker);
+      }
+  });
+  $('#makeList').on('click',function(e){
+    e.preventDefault();
+    $('#showTracker').show();
+    $('#showTracker').append(showTracker);
+  });
+  // addToLocalStorage();
+
 
   $('#submit').on('click', function(e) {
     e.preventDefault();
@@ -13,7 +43,7 @@ $(document).ready(function() {
     console.log(date);
 
 
-    var url = "https://api.bandsintown.com/events/search?format=json&app_id=Local_Sounds&api_version=2.0&location=" + city + "," + state + "&radius=" + radius + "&date="+ date;
+    var url = "https://api.bandsintown.com/events/search?format=json&app_id=Local_Sounds&api_version=2.0&location=" + city + "," + state + "&radius=" + radius + "&date=" + date;
 
     console.log(url);
 
@@ -28,24 +58,28 @@ $(document).ready(function() {
     $.ajax(settings).success(function(d) {
       // console.log(d);
       console.log("made ajax request...");
-
+console.log(d);
       var eventArray = [];
 
       for (var i = 0; i < d.length; i++) {
         var event = {};
-        event.date = '<td>' + d[i].datetime + '</td>';
+        event.interested = '<td class="interest"><input  type="checkbox"></td>';
+        event.date = '<td class="date">' + d[i].datetime + '</td>';
         event.band = '<td class="listen"><button>' + d[i].artists[0].name + '</button></td>';
-        event.venue = '<td>'+ d[i].venue.name + '<a target=_blank href ="' + d[i].ticket_url + '"><br>(Buy Tickets!)</a></td>' ;
-        event.tix = '<td> </td>';
-        event.lat = '<td>' + d[i].venue.latitude + '</td>';
-        event.long = '<td>' + d[i].venue.longitude + '</td>';
-        event.widget = '<td ><i class="fa fa-headphones"></i></td>';
-        eventArray.push(eventTR);
+        event.venue = '<td class="venue">' + d[i].venue.name + '<a target=cd_blank href ="' + d[i].ticket_url + '"><br><strong>Buy Tickets!</strong></a></td>';
+        // event.tix = '<td> </td>';
+        // event.lat = '<td>' + d[i].venue.latitude + '</td>';
+        // event.long = '<td>' + d[i].venue.longitude + '</td>';
+        // event.widget = '<td ><i class="fa fa-headphones"></i></td>';
 
-        var eventTR = '<tr>' + event.date + event.band + event.venue + '</tr>'
+        eventArray.push(eventTR);
+        // console.log(eventTR);
+        // console.log(eventArray[1]);
+
+        var eventTR = '<tr>' + event.date + event.band + event.venue + event.interested + '</tr>'
         $('tbody').append(eventTR);
       }
-      // console.log(eventArray);
+
 
       //click on band name to hear a sample of music
       $('.listen').on('click', function() {
@@ -57,11 +91,13 @@ $(document).ready(function() {
 
         play(bandName);
         var triedOnce = false;
-        function play(bandName) {
-          bandName = bandName.replace(/\s/g, '-');
 
+        function play(bandName) {
+
+          bandName = bandName.replace(/\s/g, '-');
           var bandUrl = "https://soundcloud.com/" + bandName;
           track_url = bandUrl;
+
           SC.initialize({
             client_id: '087867b3551fce712c09e156c457e95c',
             client_secret: 'd32af60ddb5057120f766d748e9da182'
@@ -77,38 +113,17 @@ $(document).ready(function() {
 
             bandName = bandName.replace(/-/g, '')
             console.log(bandName);
-            if(!triedOnce){
+            if (!triedOnce) {
               play(bandName);
-            } else{
-              alert('Sorry, can\'t find them');
+            } else {
+              alert("Sorry, we can't find this artist");
             }
             triedOnce = true;
-
           })
+
+
         }
-
       })
-
     });
-
-
-    // Embed SoundCloud Widget
-
-
-    //click on band name to hear a sample of music
-    // $('#listen').on('click',function(e) {
-    //   e.preventDefault();
-    //   console.log("band clicked!");
-    // var target = $(event.target);
-    // var bandName = target.val();
-    // console.log(bandName);
-    //
-    // var urlBand = "http://soundcloud.com/" +bandName;
-    // track_url= urlBand;
   });
-
-
-
-
-
 });
